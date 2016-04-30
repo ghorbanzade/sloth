4777c96c761b4f25fab62408eed2f4359ddf99a2
Pejman Ghorbanzade <mail@ghorbanzade.com>
2016-04-30 13:10:32 -0400

    added code for client site to retrieve data from server

---
+++ src/site/media/js/demo.js
@@ -0,0 +1,31 @@
+var app = angular.module('sloth', [], function($interpolateProvider) {
+	$interpolateProvider.startSymbol('[[');
+	$interpolateProvider.endSymbol(']]');
+});
+
+app.filter('slice', function() {
+	return function(arr, start, end) {
+		return arr.slice(start, end);
+	};
+});
+
+app.filter('capitalize', function() {
+  return function(token) {
+      return token.charAt(0).toUpperCase() + token.slice(1);
+   }
+});
+
+app.controller('demo', function($scope, $http, $location) {
+	var remote = "http://api.ghorbanzade.com";
+	$scope.loading = true;
+	$scope.url = $location.absUrl();
+	$scope.api = remote;
+	$http.get($scope.api).success(function(response) {
+		$scope.data = angular.fromJson(response);
+	}).finally(function() {
+		$scope.loading = false;
+	});
+	$scope.formatDate = function(date) {
+		return new Date(date.split("-").join("/"));
+	};
+});
