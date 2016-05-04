a8c63b70dbc0a874f62a3bf946378ddeb0a17b60
Pejman Ghorbanzade <mail@ghorbanzade.com>
2016-05-03 21:44:46 -0400

    added json serialization rules for logging activity objects

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -8,31 +8,41 @@
 package com.ghorbanzade.sloth;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 
 /**
  *
  *
  * @author Pejman Ghorbanzade
+ * @see Classifier
+ * @see Learner
  */
 public abstract class Activity {
 
-  private final Date date;
+  private final Date date = new Date();
   private final String name;
   private final double accuracy;
-  private final HashMap<Node, ActivityCode> posture;
+  private final Map<Node, ActivityCode> posture;
   private static final Logger log = Logger.getLogger(Activity.class);
+  private static final Config cfg =
+      ConfigManager.get("config/main.properties");
 
   /**
    *
@@ -40,24 +50,8 @@ public abstract class Activity {
    * @param name name of this activity
    * @param posture
    */
-  public Activity(String name, HashMap<Node, ActivityCode> posture) {
+  public Activity(String name, Map<Node, ActivityCode> posture) {
     this.name = name;
-    this.accuracy = 100;
-    this.date = new Date();
-    this.posture = posture;
-  }
-
-  /**
-   *
-   * @param name
-   * @param posture
-   * @param date
-   */
-  public Activity(
-      String name, HashMap<Node, ActivityCode> posture, Date date
-  ) {
-    this.name = name;
-    this.date = (Date) date.clone();
     this.accuracy = 100;
     this.posture = posture;
   }
@@ -72,7 +66,6 @@ public abstract class Activity {
     this.name = name;
     this.posture = null;
     this.accuracy = accuracy;
-    this.date = new Date();
   }
 
   /**
@@ -113,7 +106,7 @@ public abstract class Activity {
    *
    * @return a map of the codes received from sensor nodes
    */
-  public HashMap<Node, ActivityCode> getPosture() {
+  public Map<Node, ActivityCode> getPosture() {
     return this.posture;
   }
 
@@ -126,111 +119,166 @@ public abstract class Activity {
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append(this.date.toString());
-    sb.append(' ');
     sb.append(this.name);
     return sb.toString();
   }
 
   /**
-   *
+   * Defines an activity instance that is classified based on the activities
+   * previously learned by the system.
    *
    * @author Pejman Ghorbanzade
+   * @see Learned
    */
   public static class Classified extends Activity {
 
     /**
+     * Creates an activity object based on observed data via comparing it
+     * with the activities previously learned by the system.
      *
-     *
-     * @param name
-     * @param accuracy
+     * @param name name of activity that is learned
+     * @param accuracy the accuracy with which the activity is predicted
      */
     public Classified(String name, double accuracy) {
       super(name, accuracy);
     }
 
     /**
-     *
+     * Records a classified activity in a directory specified in
+     * configuration file using the JSON format.
      */
     @Override
     public void log() {
+      try {
+        File dir = new File(cfg.getAsString("dir.classified.activities"));
+        FileUtils.forceMkdir(dir);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmss'.json'");
+        File file = FileUtils.getFile(dir, sdf.format(new Date()));
+        Gson gson = new GsonBuilder().registerTypeAdapter(
+            Activity.Learned.class, new Activity.Serializer()
+        ).create();
+        String content = gson.toJson(this);
+        FileUtils.writeStringToFile(file, content, "UTF-8");
+      } catch (IOException ex) {
+        log.error("unable to store the classifed activity instance");
+      }
     }
 
   }
 
   /**
-   *
+   * Defines an activity instance that is learned trough real-time data
+   * collection in a laboratory setting.
    *
    * @author Pejman Ghorbanzade
+   * @see Classified
    */
   public static class Learned extends Activity {
 
-    private final Config cfg = ConfigManager.get("config/main.properties");
-
     /**
+     * Creates an activity object based on data obtained via real-time model
+     * training.
      *
-     *
-     * @param name
-     * @param posture
+     * @param name name of activity that is learned
+     * @param posture the object describing body posture during this activity
      */
-    public Learned(String name, HashMap<Node, ActivityCode> posture) {
+    public Learned(String name, Map<Node, ActivityCode> posture) {
       super(name, posture);
     }
 
     /**
-     *
-     *
-     * @param name
-     * @param posture
-     * @param date
-     */
-    public Learned(
-        String name, HashMap<Node, ActivityCode> posture, Date date
-    ) {
-      super(name, posture, date);
-    }
-
-    /**
-     *
+     * Records the activity in a directory specified in configuration file
+     * using the JSON format.
      */
     @Override
     public void log() {
       try {
-        File dir = new File(this.cfg.getAsString("dir.learned.activities"));
+        File dir = new File(cfg.getAsString("dir.learned.activities"));
         FileUtils.forceMkdir(dir);
         SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmss'.json'");
         File file = FileUtils.getFile(dir, sdf.format(new Date()));
-        String content = this.prepareContent();
+        Gson gson = new GsonBuilder().registerTypeAdapter(
+            Activity.Learned.class, new Activity.Serializer()
+        ).create();
+        String content = gson.toJson(this);
         FileUtils.writeStringToFile(file, content, "UTF-8");
       } catch (IOException ex) {
         log.error("unable to store the activity instance");
       }
     }
 
+  }
+
+  /**
+   * This class defines how an activity instance, learned or classified,
+   * should be expressed in json format.
+   *
+   * @author Pejman Ghorbanzade
+   * @see Deserializer
+   */
+  public static class Serializer implements JsonSerializer<Activity> {
+
     /**
-     * Returns a string representation of the activity which will be
-     * the content of the file to be created for that learned activity
-     * instance.
+     * Returns a json element suitable to be used to construct the file that
+     * logs an activity.
      *
-     * @return a string representation of the activity to be written in file
+     * @param act the activity object which should be serialized
+     * @param type type of the class being serialized
+     * @param context context for serialization
      */
-    private String prepareContent() {
+    @Override
+    public JsonElement serialize(Activity act, Type type,
+        JsonSerializationContext context
+    ) {
+      if (act instanceof Activity.Learned) {
+        return serializeLearned(act);
+      } else if (act instanceof Activity.Classified) {
+        return serializeClassified(act);
+      } else {
+        throw new UnsupportedOperationException();
+      }
+    }
+
+    /**
+     * Returns a json element for the given activity which will determine the
+     * content of the file to be created for that learned activity instance.
+     *
+     * @param act the activity object which should be serialized
+     * @return a json element with structure suitable to be written to file
+     */
+    private JsonElement serializeLearned(Activity act) {
+      JsonObject obj = new JsonObject();
+      obj.addProperty("name", act.getName());
       JsonArray posture = new JsonArray();
-      for (Node node: this.getPosture().keySet()) {
-        JsonObject bodyPart = new JsonObject();
-        bodyPart.addProperty("node", node.getId());
+      for (Node node: act.getPosture().keySet()) {
+        JsonObject part = new JsonObject();
+        part.addProperty("id", node.getId());
         JsonArray code = new JsonArray();
-        for (double num: this.getPosture().get(node).getCode()) {
+        for (double num: act.getPosture().get(node).getCode()) {
           code.add(Math.round(num * 1000) / 1000.0);
         }
-        bodyPart.add("code", code);
-        posture.add(bodyPart);
+        part.add("code", code);
+        posture.add(part);
       }
-      JsonObject msg = new JsonObject();
-      msg.addProperty("name", this.getName());
-      msg.add("posture", posture);
-      Gson gson = new Gson();
-      return gson.toJson(msg);
+      obj.add("posture", posture);
+      return obj;
+    }
+
+    /**
+     * Returns a json element for the given activity which will determine the
+     * content of the file to be created for that classified activity instance.
+     *
+     * @param act the activity object which should be serialized
+     * @return a json element with structure suitable to be written to file
+     */
+    private JsonElement serializeClassified(Activity act) {
+      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ");
+      df.setTimeZone(TimeZone.getTimeZone("UTC"));
+      JsonObject obj = new JsonObject();
+      obj.addProperty("name", act.getName());
+      obj.addProperty("accuracy", act.getAccuracy());
+      obj.addProperty("date", df.format(new Date()));
+      return obj;
     }
 
   }
--- src/main/java/Classifier.java
+++ src/main/java/Classifier.java
@@ -7,30 +7,69 @@
 
 package com.ghorbanzade.sloth;
 
+import com.google.gson.Gson;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
  *
  * @author Pejman Ghorbanzade
- * @see ActivityQueue
- * @see Queue
+ * @see Activity
+ * @see Learner
+ * @see Posture
  */
 public final class Classifier implements Runnable {
 
   private final Config cfg;
   private final Posture posture;
+  private final ArrayList<Activity> acts;
   private static final Logger log = Logger.getLogger(Classifier.class);
 
   /**
    *
    *
    * @param posture the posture based on which classification should be made
-   * @param aq the queue on which classified activity should be put
+   * @throws FatalException if learned activity samples cannot be loaded
    */
-  public Classifier(Posture posture) {
+  public Classifier(Posture posture) throws FatalException {
     this.posture = posture;
     this.cfg = ConfigManager.get("config/main.properties");
+    try {
+      this.acts = load();
+    } catch (IOException ex) {
+      throw new FatalException(Classifier.class);
+    }
+  }
+
+  /**
+   * Loads all learned models as a list of learned activities to be used
+   * later for classification.
+   *
+   * @return a list of activities previously learned
+   */
+  private ArrayList<Activity> load() throws IOException {
+    File dir = new File(this.cfg.getAsString("dir.learned.activities"));
+    FileUtils.forceMkdir(dir);
+    String[] exts = {"json"};
+    Iterator<File> it = FileUtils.iterateFiles(dir, exts, true);
+    ArrayList<Activity> acts = new ArrayList<Activity>();
+    Gson gson = new Gson();
+    while (it.hasNext()) {
+      File file = it.next();
+      String content = FileUtils.readFileToString(file, "UTF-8");
+      Activity act = gson.fromJson(content, Activity.Learned.class);
+      System.out.println(act.getName());
+      acts.add(act);
+    }
+    return acts;
   }
 
   /**
@@ -38,7 +77,27 @@ public final class Classifier implements Runnable {
    */
   @Override
   public void run() {
-    // TODO
+    while (!Thread.currentThread().isInterrupted()) {
+      try {
+        Thread.sleep(this.cfg.getAsInt("classifier.sleep.interval"));
+        this.classify();
+      } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+        log.info("sleep interrupted");
+      }
+    }
+    log.info("classifier stopped by the main thread");
+  }
+
+  /**
+   *
+   */
+  private void classify() {
+    String name = "Biking";
+    double accuracy = 96.7;
+    Activity act = new Activity.Classified(name, accuracy);
+    this.posture.reset();
+    act.log();
   }
 
 }
