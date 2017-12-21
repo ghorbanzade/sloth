245b46b3043c115b740ed326fbe3009a7c84aa8b
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2017-12-20 21:28:46 -0600

    renamed dev to tools

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -14,7 +14,7 @@ task_codacy () {
         log_error "codacy project token is not set"
         return;
     fi
-    wget -O ~/codacy-coverage-reporter-assembly-latest.jar $(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
+    wget -q -O ~/codacy-coverage-reporter-assembly-latest.jar $(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
     [ "${TRAVIS_PULL_REQUEST}" = "false" ] && java -cp ~/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter -l Java -r build/reports/jacoco/test/jacocoTestReport.xml
 }
 
