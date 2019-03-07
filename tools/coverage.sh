64aaafa377edc7992ef521f68640c49f1c973896
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-03-06 22:36:15 -0600

    minor improvements and code cleanups

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-__log () {
-    if [ $# -lt 2 ]; then return; fi
-    local msg="${@:2}"
-    printf "%-10s %s\n" $1 "${msg}"
-}
-log_debug () { [[ ${DEBUG} -eq 1 ]] && __log 'debug' $@; }
-log_info  () { __log 'info' $@; }
-log_error () { __log 'error' $@; }
-
 task_codacy () {
-    if [ -z ${CODACY_PROJECT_TOKEN} ]; then
-        log_error "codacy project token is not set"
-        return;
+    if [ -z "${CODACY_PROJECT_TOKEN}" ]; then
+        echo "codacy project token is not set"
+        exit 1
     fi
-    wget -q -O ~/codacy-coverage-reporter-assembly-latest.jar $(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
+    wget -q -O ~/codacy-coverage-reporter-assembly-latest.jar "$(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)"
     [ "${TRAVIS_PULL_REQUEST}" = "false" ] && java -cp ~/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter -l Java -r build/reports/jacoco/test/jacocoTestReport.xml
 }
 
@@ -25,11 +16,11 @@ task_codecov () {
 for arg in "$@"; do
     case $arg in
         codacy)
-            log_debug "reporting coverage to codacy"
+            echo "reporting coverage to codacy"
             task_codacy
             ;;
         codecov)
-            log_debug "reporting coverage to codecov"
+            echo "reporting coverage to codecov"
             task_codecov
             ;;
     esac
